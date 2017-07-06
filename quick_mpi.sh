137c13f9be01eb2252bd88c19e7ec4a15344a0d6
Mathew Madhavacheril <mathewsyriac@gmail.com>
2017-07-06 19:03:04 -0400

    working quick_mpi

--- quick_mpi.sh
+++ quick_mpi.sh
@@ -1,10 +1,24 @@
 #/bin/bash
 
+trim() {
+    local var="$*"
+    # remove leading whitespace characters
+    var="${var#"${var%%[![:space:]]*}"}"
+    # remove trailing whitespace characters
+    var="${var%"${var##*[![:space:]]}"}"   
+    echo -n "$var"
+}
 
 nowvar=$(date +"%s")
 
-#echo ${1}
-#echo ${@:2}
+# echo ${1}
+# echo ${@:2}
+# echo $(pwd)
 
 
-nohup wq sub -r "mode:bycore;N:${1};hostfile: auto;job_name: mpi;priority:med" -c "source ~/.bash_profile ; source ~/.bashrc ; cd $(pwd) ; mpirun -hostfile %hostfile% ${@:2} "  > $(pwd)/output$nowvar.log 2>&1 &
+my_prog=$(trim "${@:2}")
+# echo $my_prog
+
+nohup wq sub -r "mode:bycore;N:${1};hostfile: auto;job_name:mpi_test;priority:med;group:[gen6]" -c "source ~/.bash_profile ; source ~/.bashrc ; cd $(pwd) ; mpirun -hostfile %hostfile% ${my_prog} " > $(pwd)/outputnow.log 2>&1  &
+
+
