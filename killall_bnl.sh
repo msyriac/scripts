0ae0af0fe228418ba0b03bec184fe55d9d019b2f
Mathew Madhavacheril <mathewsyriac@gmail.com>
2017-12-17 13:27:53 -0500

    git status script

--- killall_bnl.sh
+++ killall_bnl.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for mach in $(wq stat | grep astro | awk '{print $2}'); do ssh $mach "echo $mach ; killall python"; done &> msyriac-jobs-killall
+for mach in $(wq stat | grep astro | awk '{print $2}'); do ssh $mach "echo $mach ; killall python; killall orted; killall mpirun"; done &> msyriac-jobs-killall
