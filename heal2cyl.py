daf520acb5ffc83f49db7ecc3d929e2d4503fbdd
Mathew Madhavacheril <mathewsyriac@gmail.com>
2018-03-18 12:29:59 -0400

    fits_info

--- heal2cyl.py
+++ heal2cyl.py
@@ -6,21 +6,27 @@ parser = argparse.ArgumentParser()
 parser.add_argument("ifile")
 parser.add_argument("ofile")
 parser.add_argument("-r", "--res", type=float, default=None,help="Resolution in arcminutes.")
+parser.add_argument("-t", "--template", type=str, default=None,help="Template for output.")
 parser.add_argument("-c", "--coords", type=str, default="j2000")
 args = parser.parse_args()
 
 imap  = hp.read_map(args.ifile)
 assert imap.ndim==1, "Can't convert multi-component healpix maps"
 
-if args.res is None:
-    npix = imap.size
-    full_sky_radian_squares = 4.*np.pi
-    area_pix = full_sky_radian_squares/npix
-    res = np.sqrt(area_pix)
+if args.template is not None:
+    assert args.res is None
+    shape,wcs = enmap.read_map_geometry(args.template)
 else:
-    res = args.res * np.pi/180./60.
+
+    if args.res is None:
+        npix = imap.size
+        full_sky_radian_squares = 4.*np.pi
+        area_pix = full_sky_radian_squares/npix
+        res = np.sqrt(area_pix)
+    else:
+        res = args.res * np.pi/180./60.
     
-shape, wcs = enmap.fullsky_geometry(res=res, shape=None, dims=(), proj="car")
+    shape, wcs = enmap.fullsky_geometry(res=res, shape=None, dims=(), proj="car")
 print(shape)
 
 omap = maps.enmap_from_healpix(shape,wcs,imap,hp_coords=args.coords,interpolate=True)
