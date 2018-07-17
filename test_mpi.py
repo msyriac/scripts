7a562ffa0eaf6b79280bbfda046d6811bbba64cb
Mathew Madhavacheril <mathewsyriac@gmail.com>
2018-07-17 16:13:17 -0400

    tiger MPI script and template

--- test_mpi.py
+++ test_mpi.py
@@ -1,7 +1,31 @@
 from __future__ import print_function
 from mpi4py import MPI
+from orphics import stats,mpi
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 numcores = comm.Get_size()
 print("Rank = ",rank, " / ",numcores)
+
+
+
+Njobs = 80
+num_each,each_tasks = mpi.mpi_distribute(Njobs,numcores)
+if rank==0: print ("At most ", max(num_each) , " tasks...")
+my_tasks = each_tasks[rank]
+
+s = stats.Stats(comm)
+
+for task in my_tasks:
+
+    s.add_to_stats("count",(1,))
+    
+    if rank==0: print ("Rank 0 done with task ", task+1, " / " , len(my_tasks))
+
+s.get_stats()
+
+if rank==0:
+
+    tot = s.vectors['count'].sum()
+    print(tot)
+    assert tot==Njobs
--- tiger_template.sh
+++ tiger_template.sh
@@ -9,4 +9,12 @@
 export OMP_NUM_THREADS=!THREADS
 export DISABLE_MPI=false
 
+module load anaconda/5.0.1
+module load intel-mkl/2018.3/3/64
+module load intel/18.0/64/18.0.3.222
+module load intel-mpi/intel/2018.3/64
+module load fftw/intel-16.0/intel-mpi/3.3.4
+module load mpi4py/gcc/intel-mpi/2.0.0
+
+
 srun !CMD
